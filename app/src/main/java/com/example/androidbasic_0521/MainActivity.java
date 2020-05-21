688c651292fbdb6ce5725a4f97cf2d14e47a665a
kimhaneui <kimhaneui2@naver.com>
2020-05-21 11:29:13 +0900

    binding 처리

--- app/src/main/java/com/example/androidbasic_0521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic_0521/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.androidbasic_0521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
+import android.database.DatabaseUtils;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic_0521.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvent();
         setValues();
     }
 //    이벤트 처리코드 모아두는곳
     void setupEvent(){
+
+        binding.goToFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
