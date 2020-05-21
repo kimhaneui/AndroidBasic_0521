edf5e028a9902444b28984d20e21e45b7c5eb5b9
kimhaneui <kimhaneui2@naver.com>
2020-05-21 11:12:41 +0900

    액티비티 내부 코드정리

--- app/src/main/java/com/example/androidbasic_0521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic_0521/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupEvent();
+        setValues();
+    }
+//    이벤트 처리코드 모아두는곳
+    void setupEvent(){
+
+    }
+
+//    화면에 나와야할 데이터세팅 코드 모아두는 곳
+    void setValues(){
+
     }
 }
